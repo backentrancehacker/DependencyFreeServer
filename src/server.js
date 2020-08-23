e8ed918209b463728c8a5a91fedc969c7e6d7825
NathanFS <backentrancehacker@gmail.com>
2020-08-23 10:26:16 +0000

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,141 @@
+const { createServer } = require('http')
+const { createReadStream } = require('fs')
+
+const path = require('path')
+const url = require('url')
+const query = require('querystring')
+
+const utils = require('./utils')
+
+class Server {
+	constructor() {
+		this.routes = this.config('get', 'post')
+		this.server = this.initialize()
+
+		return this
+	}
+	serve(dir) {
+		utils.walk(dir).then(files => {
+			files.forEach(file => {
+				this.all(file.replace(dir, ''), (req, res) => res.sendFile(file))
+			})
+		})
+	}
+	process(cb) {
+		return ((...params) => {
+			const [req, res] = params
+			try {
+				cb.apply(null, params)
+			}
+			catch(e) {
+				const { pathname } = url.parse(req.url)				
+				const exec = this.routes[req.method]['500'] || ((req, res) => res.status(500).send('Internal server error'))
+				
+				exec(req, res, e)
+			}
+		})
+	}
+	all(pathname, cb) {
+		for(const route in this.routes) {
+			this.routes[route][pathname] = this.process(cb)
+		}
+	}
+	config(...types) {
+		let routes = {}
+		for(let type of types) {
+			routes[type.toUpperCase()] = {}
+			Server.prototype[type] = (pathname, cb) => {
+				if(typeof pathname == 'string') pathname = [pathname]
+				for(const r of pathname) {
+					this.routes[type.toUpperCase()][r.toString()] = this.process(cb)
+				}
+			}
+		}
+		return routes
+	}
+	resMethods(res) {
+		const add = {
+			status: code => {
+				res.statusCode = code
+				return Object.assign({}, res, add)
+			},
+			send: (data, headers) => {
+				for(const key in headers) {
+					res.setHeader(key, headers[key]);
+				}
+				if(typeof data == 'string') {
+					res.end(data, 'utf-8')
+				}
+				else {
+					res.end(JSON.stringify(data), 'utf-8')
+				}
+			},
+			redirect: (pathname) => {
+				add.status(302).send(null, {
+					'Location': pathname
+				})
+			},
+			sendFile: (file) => {
+				const ext = String(path.extname(file))
+				const content = utils.type[ext] || 'application/octet-stream'
+				const headers = {
+					'Content-Type': content
+				}
+				res.writeHead(200, headers)
+				if (fs.existsSync(file)) {
+					createReadStream(file).pipe(res)				
+				}
+				else {
+					throw new Error('File does not exist')
+				}
+			}
+		}
+		return add
+	}
+	async reqMethods(req) {
+		const add = {}
+
+		if(req.method == 'POST') {
+			const body = await new Promise((resolve, reject) => {
+				let data = []
+				req.on('data', chunk => {
+					data.push(chunk)
+				})
+				req.on('end', () => {
+					resolve(data)
+				})
+			})
+			switch(req.headers['Content-Type']) {
+				case 'application/x-www-form-urlencoded': 
+					add.query = query.parse(body)
+					break
+				case 'application/json': 
+					add.body = JSON.parse(body)
+					break
+				default:
+					add.body = body.toString()
+			}
+		}
+		
+		return add
+	}
+	initialize() {
+		const server = createServer()
+
+		server.on('request', async (req, res) => {
+			const { pathname } = url.parse(req.url)
+			const exec = this.routes[req.method][pathname || '404'] || ((req, res) => res.status(404).send(`Could not ${req.method} ${pathname}`))
+			
+			Object.assign(res, this.resMethods(res))
+			Object.assign(req, await this.reqMethods(req))
+			exec(req, res)
+		})
+
+		return server
+	}
+	listen(port, cb) {
+		this.server.listen(port, cb || null)
+	}
+}
+
+module.exports = Server
---
+++ src/utils.js
@@ -0,0 +1,55 @@
+const fs = require('fs').promises
+const path = require('path')
+
+const walk = async dir => {
+	let files = await fs.readdir(dir)
+	files = await Promise.all(files.map(async file => {
+		const filePath = path.join(dir, file)
+		const stats = await fs.stat(filePath)
+		if(stats.isDirectory()) return walk(filePath)
+		else if(stats.isFile()) return filePath
+	}))
+	return files.reduce((all, folders) => all.concat(folders), [])
+}
+
+const cookies = {
+	parse: (cookie) => {
+		let r = /([^;=\s]*)=([^;]*)/g
+		let obj = {}
+		for(let m; m = r.exec(cookie);) {
+			obj[m[1]] = decodeURIComponent(m[2])
+		}
+		return obj
+	},
+	stringify: (cookie) => {
+		let list = []
+		for(const [key, value] of Object.entries(cookie)) {
+			list.push(`${key}=${encodeURIComponent(value)}`)
+		}
+		return list.join('; ')
+	}
+}
+
+const type = {
+	'.html': 'text/html',
+	'.js':   'text/javascript',
+	'.css':  'text/css',
+	'.json': 'application/json',
+	'.png':  'image/png',
+	'.jpg':  'image/jpg',
+	'.gif':  'image/gif',
+	'.svg':  'image/svg+xml',
+	'.wav':  'audio/wav',
+	'.mp4':  'video/mp4',
+	'.woff': 'application/font-woff',
+	'.ttf':  'application/font-ttf',
+	'.eot':  'application/vnd.ms-fontobject',
+	'.otf':  'application/font-otf',
+	'.wasm': 'application/wasm'
+}
+
+module.exports = {
+	walk,
+	cookies,
+	type
+}
